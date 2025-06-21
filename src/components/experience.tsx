66ac8d0a56fa6babd7aab9b8659d3a968acd85c8
anuragprasoon <anuragprasoon23@gmail.com>
2025-06-21 19:02:09 +0530

    v1.01

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -1,6 +1,41 @@
 import React from "react"
-const Experience: React.FC=()=>{
-    return null
+const Experience: React.FC<{}>=()=>{
+    return(<div className="w-full sm:flex bg-white text-black">
+<div className="urbanist p-10 w-[400px] sm:w-[50%]">
+<div className="text-[16px] text-[#E4A689]">EXPERIENCE</div>
+<div className="text-[18px]">
+<h3 className="text-[40px] font-extrabold">Experience A Luxury Wellness Retreat in India</h3>
+Step away from the rush and return to what matters. Nestled in nature, this retreat offers space to slow down, reconnect, and recharge—from mindful movement and nourishing meals to meaningful connection. Come home to yourself.
+<button className="block bg-[#2E524A] text-white p-5 text-[12px] font-bold rounded-full mt-5">Book Now</button>
+</div>
+</div>
+
+<div className="sm:flex p-10 gap-5 w-[50%] justify-center items-center text-black">
+<div className="relative w-[200px] h-[300px] rounded-full urbanist">
+<img className="object-cover rounded" src="https://res.cloudinary.com/anuragprasoon/image/upload/v1750478607/sacred-pause-com/nuqph0eifartcwkubtme.png"/>
+<span className="text-white text-[20px] absolute bottom-10 text-center w-full p-3">Morning Yoga</span>
+</div>
+
+<div className="relative w-[200px] h-[300px] rounded-full urbanist">
+<img className="object-cover rounded" src="https://res.cloudinary.com/anuragprasoon/image/upload/v1750478607/sacred-pause-com/nuqph0eifartcwkubtme.png"/>
+<span className="text-white text-[20px] absolute bottom-10 text-center w-full p-3">Morning Yoga</span>
+</div>
+
+<div className="relative w-[200px] h-[300px] rounded-full urbanist">
+<img className="object-cover rounded" src="https://res.cloudinary.com/anuragprasoon/image/upload/v1750478607/sacred-pause-com/nuqph0eifartcwkubtme.png"/>
+<span className="text-white text-[20px] absolute bottom-10 text-center w-full p-3">Morning Yoga</span>
+</div>
+
+<div className="relative w-[200px] h-[300px] rounded-full urbanist">
+<img className="object-cover rounded" src="https://res.cloudinary.com/anuragprasoon/image/upload/v1750478607/sacred-pause-com/nuqph0eifartcwkubtme.png"/>
+<span className="text-white text-[20px] absolute bottom-10 text-center w-full p-3">Morning Yoga</span>
+</div>
+
+</div>
+
+</div>
+
+)
 }
 
 export default Experience;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,13 @@ import Image from "next/image";
 import WhyUs from "@/components/whyus";
 import BenefitsSection from "@/components/benefits";
 import Hero from "@/components/hero";
+import Experience from "@/components/experience";
 
 export default function Home() {
   return (
     <>
     <Hero/>
+    <Experience/>
     <BenefitsSection/>
     <WhyUs/>
     </>
