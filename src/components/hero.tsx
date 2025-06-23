d59de627c461a3b4ccb6d790feaa7b80d1991447
anuragprasoon <anuragprasoon23@gmail.com>
2025-06-24 05:09:10 +0530

    hero sec minor changes

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -5,8 +5,8 @@ const Hero: React.FC=()=>{
         <div className="relative flex justify-center items-center  max-sm:h-[800px]  bg-[#172525] max-sm:pb-[200px]"><center>
 <img className="w-full object-fit absolute left-1/2 transform -translate-x-1/2" src="https://res.cloudinary.com/anuragprasoon/image/upload/v1750478606/sacred-pause-com/u79divlpdfdksva1msbd.png"/>
 <img className="sm:w-[95%] h-[600px] object-cover anim" src="https://res.cloudinary.com/anuragprasoon/image/upload/v1750719350/sacred-pause-com/mhnziq8puwaj9fae88bk.png"/>
-<div className="absolute top-[30%]  w-full  text-[16px]  text-white  urbanist left-1/2 transform -translate-x-1/2 p-10">
-<h1 className=" font-bold text-[60px] max-sm:text-[50px] text-white ">RETREAT. RESTORE. RISE.</h1>
+<div className="absolute sm:top-[30%] top-[20%]  w-full  text-[16px]  text-white  urbanist left-1/2 transform -translate-x-1/2 p-10">
+<h1 className=" font-bold text-[60px] max-sm:text-[40px] text-white ">RETREAT. RESTORE. RISE.</h1>
 A transformational luxury yoga and wellness retreat in India for those ready to pause, reconnect, and renew.
 <button className="block p-3 bg-white text-black mt-5 rounded-full font-semibold">Book Now</button>
 </div>
