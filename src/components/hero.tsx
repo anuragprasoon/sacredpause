92ce4c078e930b93751c37899ec7fa54a905d270
anuragprasoon <anuragprasoon23@gmail.com>
2025-06-24 05:06:50 +0530

    hero sec minor change

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Hero: React.FC=()=>{
     return(
-        <div className="relative flex justify-center items-center  max-sm:h-[1000px]  bg-[#172525] max-sm:pb-[200px]"><center>
+        <div className="relative flex justify-center items-center  max-sm:h-[800px]  bg-[#172525] max-sm:pb-[200px]"><center>
 <img className="w-full object-fit absolute left-1/2 transform -translate-x-1/2" src="https://res.cloudinary.com/anuragprasoon/image/upload/v1750478606/sacred-pause-com/u79divlpdfdksva1msbd.png"/>
 <img className="sm:w-[95%] h-[600px] object-cover anim" src="https://res.cloudinary.com/anuragprasoon/image/upload/v1750719350/sacred-pause-com/mhnziq8puwaj9fae88bk.png"/>
 <div className="absolute top-[30%]  w-full  text-[16px]  text-white  urbanist left-1/2 transform -translate-x-1/2 p-10">
